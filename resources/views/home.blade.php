de250d75cf4243a00b47432db9519bc262b42eb6
Melani <melanigallego@gmail.com>
2021-02-16 19:41:31 +0100

    Página home
    Añadimos la imagen y mostramos datos en la página home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,8 @@
 @include('header')
+
+<div class="container justify-content-center">
+<h3 style="margin-top: 160px;">Home de la web</h3>
+<img src="imagen.jpg" width="400" height="341">
+</div>
+
 @include('footer')
