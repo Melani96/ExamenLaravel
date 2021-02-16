7b18b12a046a43b1cf84d38b3abce67739ed0a53
Melani <melanigallego@gmail.com>
2021-02-16 19:33:58 +0100

    Añadiendo los primeros pasos
    Añadido header, footer y página de home vacia (solo contiene el header y footer)

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">Melania Gallego Otero 2GS 16/02/2021</span>
+  </div>
+</footer>
+
+
+    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+      
+  </body>
+</html>
